5f5ef01d33f2f13576836e0ecfc20307e92ec5be
Amit-987 <jgcbfdgv@gmail.com>
2025-11-13 14:06:44 +0500

    feat: add src/modes/init.py

---
+++ src/modes/init.py
@@ -0,0 +1,11 @@
+"""Mode runners for the different scraping entry points."""
+
+from . import search_mode, trend_mode, hashtag_mode, user_mode, music_mode
+
+__all__ = [
+"search_mode",
+"trend_mode",
+"hashtag_mode",
+"user_mode",
+"music_mode",
+]
